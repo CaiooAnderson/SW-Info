26f84c2dea02bb91497c94552c44d4047fa7df0e
CaiozinS2 <caioandersongoes@gmail.com>
2025-01-13 17:43:27 -0300

    Criando tabela no componente filmes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { InicioComponent } from './inicio/inicio.component';
 import { FilmesComponent } from './filmes/filmes.component';
 import { NavesComponent } from './naves/naves.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,8 +18,11 @@ import { NavesComponent } from './naves/naves.component';
     NavesComponent,
   ],
   imports: [
+    MatTableModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/filmes/filmes.component.ts
+++ src/app/filmes/filmes.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilmesService } from '../services/filmes.service';
 
 @Component({
   selector: 'app-filmes',
   templateUrl: './filmes.component.html',
-  styleUrls: ['./filmes.component.scss']
+  styleUrls: ['./filmes.component.scss'],
 })
-export class FilmesComponent {
+export class FilmesComponent implements OnInit {
+  displayedColumns: string[] = ['title', 'release_date', 'director'];
+  filmes: any[] = [];
 
+  constructor(private filmesService: FilmesService) {}
+
+  ngOnInit(): void {
+    this.filmesService.getFilmes().subscribe({
+      next: (data: any[]) => {
+        this.filmes = data;
+        console.log(this.filmes);
+      },
+      error: (err) => {
+        console.error('Erro ao carregar os filmes:', err);
+      },
+    });
+  }
 }
