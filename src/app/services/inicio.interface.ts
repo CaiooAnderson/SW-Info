5586f7245b2cc7a5e70df81065e1a259a19964aa
CaiozinS2 <caioandersongoes@gmail.com>
2025-02-14 16:15:42 -0300

    Adicionado titulo na interface e função de busca otimizada no service

--- src/app/services/inicio.interface.ts
+++ src/app/services/inicio.interface.ts
@@ -1,6 +1,7 @@
 export interface Personagem {
     name: string;
     species: string;
+    title: string;
     gender: string;
     img: string;
 }
--- src/app/services/inicio.service.ts
+++ src/app/services/inicio.service.ts
@@ -10,28 +10,21 @@ import { Personagem } from './inicio.interface';
 export class InicioService {
   private apiUrl = 'https://swapi.dev/api/people/';
   private isSearchActive = new BehaviorSubject<boolean>(false);
+  private speciesMap: { [key: string]: string } = {};
 
   constructor(private http: HttpClient) {}
 
   buscarTodosOsPersonagens(): Observable<Personagem[]> {
-    const fetchPage = (url: string): Observable<Personagem[]> => {
-      return this.http.get<{ results: any[], next: string | null }>(url).pipe(
-        switchMap(response => {
-          const personagensObservables = response.results.map(character => this.processarPersonagem(character));
-          return forkJoin(personagensObservables).pipe(
-            switchMap(personagens => {
-              if (response.next) {
-                return fetchPage(response.next).pipe(map(nextPageResults => [...personagens, ...nextPageResults]));
-              } else {
-                return of(personagens);
-              }
-            })
-          );
-        })
-      );
-    };
+    return this.buscarTodasEspecies().pipe(
+      switchMap(speciesList => {
+        this.speciesMap = speciesList.reduce((map, species) => {
+          map[species.url] = species.name;
+          return map;
+        }, {});
   
-    return fetchPage(this.apiUrl);
+        return this.fetchAllCharacters();
+      })
+    );
   }
 
   buscarTodasEspecies(): Observable<any[]> {
@@ -91,33 +84,50 @@ export class InicioService {
 
   private processarPersonagem(character: any): Observable<Personagem> {
     const speciesUrl = character.species[0];
-
+  
     return speciesUrl
       ? this.http.get<{ name: string }>(speciesUrl).pipe(
           map(speciesData => ({
             name: character.name,
-            img: this.getPersonagemImage(character.name),
+            img: this.getPersonagemImage(character.name).img, 
+            title: this.getPersonagemImage(character.name).title,
             species: speciesData.name || 'Desconhecido',
             gender: character.gender || 'Desconhecido',
           }))
         )
       : of({
           name: character.name,
-          img: this.getPersonagemImage(character.name),
+          img: this.getPersonagemImage(character.name).img,
+          title: this.getPersonagemImage(character.name).title,
           species: 'Desconhecido',
           gender: character.gender || 'Desconhecido',
         });
   }
 
-  private getPersonagemImage(name: string): string {
-    const images: { [key: string]: string } = {
-      'Luke Skywalker': '../../assets/characters/Luke_Skywalker.svg',
-      'Darth Vader': '../../assets/characters/Darth_Vader.svg',
-      'Leia Organa': '../../assets/characters/Leia_Organa.svg',
-      'Obi-Wan Kenobi': '../../assets/characters/Obi_Wan_Kenobi.svg',
-      'Yoda': '../../assets/characters/Yoda.svg',
+  private getPersonagemImage(name: string): { img: string, title: string } {
+    const images: { [key: string]: { img: string, title: string } } = {
+      'Luke Skywalker': { 
+        img: '../../assets/characters/Luke_Skywalker.svg',
+        title: 'Jedi'
+      },
+      'Darth Vader': {
+        img: '../../assets/characters/Darth_Vader.svg',
+        title: 'Sith'
+      },
+      'Leia Organa': {
+        img: '../../assets/characters/Leia_Organa.svg',
+        title: 'Rebelde'
+      },
+      'Obi-Wan Kenobi': {
+        img: '../../assets/characters/Obi_Wan_Kenobi.svg',
+        title: 'Jedi'
+      },
+      'Yoda': {
+        img: '../../assets/characters/Yoda.svg',
+        title: 'Jedi'
+      },
     };
-    return images[name] || '../../assets/characters/Unknown_Character.svg';
+    return images[name] || { img: '../../assets/characters/Unknown_Character.svg', title: 'Desconhecido' };
   }
 
   private getUniqueRandomIndexes(count: number, min: number, max: number): number[] {
@@ -126,5 +136,22 @@ export class InicioService {
       indexes.add(Math.floor(Math.random() * (max - min + 1)) + min);
     }
     return Array.from(indexes);
+  }
+
+  private fetchAllCharacters(): Observable<Personagem[]> {
+    const totalPages = 9;
+    const requests = [];
+  
+    for (let i = 1; i <= totalPages; i++) {
+      requests.push(this.http.get<{ results: any[] }>(`${this.apiUrl}?page=${i}`));
+    }
+  
+    return forkJoin(requests).pipe(
+      map(responses => responses.flatMap(response => response.results)),
+      switchMap(personagens => {
+        const personagensCompletos = personagens.map(character => this.processarPersonagem(character));
+        return forkJoin(personagensCompletos);
+      })
+    );
   }
 }
